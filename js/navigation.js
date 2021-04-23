d178cde29208ab185b536459d4abd2ce2b89ee81
Benjamín Martinez <benjaminmq2013@gmail.com>
2021-04-23 11:42:15 -0300

    Navigation improved

---
+++ js/navigation.js
@@ -0,0 +1,7 @@
+const create_image = document.querySelector(".create_image");
+
+create_image.addEventListener("click", iniciar_compra)
+
+function iniciar_compra(){
+    window.open("https://benjaminmq2013.github.io/pizza_store/pages/buy.html","_self")
+}
